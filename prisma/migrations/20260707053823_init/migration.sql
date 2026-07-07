86bba76037cdb14d1d65dbd109fc15f456c8094a
nshakib <nazmus.shakib.cse@gmail.com>
2026-07-07 11:47:39 +0600

    chore: initial project scaffolding with TypeScript, Prisma, and User model

---
+++ prisma/migrations/20260707053823_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "ActiveStatus" AS ENUM ('ACTIVE', 'BLOCKED');
+
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('TENANT', 'LANDLORD', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "activeStatus" "ActiveStatus" NOT NULL DEFAULT 'ACTIVE',
+    "role" "Role" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
