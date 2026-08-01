ce4c22f8c270c66a9255248c90ca568b45229195
nshakib <nazmus.shakib.cse@gmail.com>
2026-08-02 03:47:09 +0600

    fix: add local app url

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,7 +6,7 @@ dotenv.config({path: path.join(process.cwd(), ".env") });
 export default {
     port : process.env.PORT,
     database_url : process.env.DATABASE_URL,
-    app_url : process.env.APP_URL,
+    app_url : process.env.APP_URL || "http://localhost:3000",
     bcrypt_salt_rounds : process.env.BCRYPT_SALT_ROUNDS!,
     jwt_access_secret : process.env.JWT_ACCESS_SECRET!,
     jwt_refresh_secret : process.env.JWT_REFRESH_SECRET!,
